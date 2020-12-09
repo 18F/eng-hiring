39aad6e1e7b26ea5b906687f95f8c28b830100f2
Laura Gerhardt <laura.gerhardt@gsa.gov>
2020-12-09 13:34:02 -0500

    update footer call to https

--- _plugins/copy_18f_footer.rb
+++ _plugins/copy_18f_footer.rb
@@ -5,15 +5,15 @@ module Jekyll
     def generate(site)
         #get 18f footer
         u = URI('https://raw.githubusercontent.com/18F/18f.gsa.gov/main/_includes/navigation/footer.html')
-        Net::HTTP.start(u.host, u.port) do |http|
+        Net::HTTP.start(u.host, u.port, :use_ssl => true) do |http|
             request = Net::HTTP::Get.new(u)
             http.request(request) do |response|
             open("_includes/footer.html", "wb") do |file|
               response.read_body do |chunk|
-                puts chunk
                 file.write(chunk)
               end
             end
+          end
         end
         puts "Done."
     end
