3f18100791d7ff49058ec92890afb73e2c1cffd8
Laura Gerhardt <laura.gerhardt@gsa.gov>
2020-12-10 11:46:03 -0500

    update hash refs

--- _plugins/copy_18f_footer.rb
+++ _plugins/copy_18f_footer.rb
@@ -6,18 +6,23 @@ module Jekyll
         #get 18f footer
         base_uri = 'https://raw.githubusercontent.com/18F/18f.gsa.gov/main/'
         assets = [{dest:'_includes/footer.html', filename:'_includes/navigation/footer.html'}]
-        u = URI(based_uri + assets[0].filename)
-        Net::HTTP.start(u.host, u.port, :use_ssl => true) do |http|
-            request = Net::HTTP::Get.new(u)
-            http.request(request) do |response|
-            open(assets[0].dest, "wb") do |file|
-              response.read_body do |chunk|
-                file.write(chunk)
-              end
+
+        grab_remote_file(assets[0], base_uri)
+    end
+
+    def grab_remote_file(file_attr, base_uri)
+      u = URI(base_uri + file_attr[:filename])
+      Net::HTTP.start(u.host, u.port, :use_ssl => true) do |http|
+          request = Net::HTTP::Get.new(u)
+          http.request(request) do |response|
+          open(file_attr[:dest], "wb") do |file|
+            response.read_body do |chunk|
+              file.write(chunk)
             end
           end
         end
-        puts "Done."
+      end
+      puts "Done."
     end
   end
 end
