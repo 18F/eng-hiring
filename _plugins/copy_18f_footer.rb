39afb3864aa4dc3aefe6ae3ca5bb585e2ef6874f
Laura Gerhardt <laura.gerhardt@gsa.gov>
2020-12-10 11:37:29 -0500

    test referential footer grab

--- _plugins/copy_18f_footer.rb
+++ _plugins/copy_18f_footer.rb
@@ -4,11 +4,13 @@ module Jekyll
   class JekyllCopyUswds < Generator
     def generate(site)
         #get 18f footer
-        u = URI('https://raw.githubusercontent.com/18F/18f.gsa.gov/main/_includes/navigation/footer.html')
+        base_uri = 'https://raw.githubusercontent.com/18F/18f.gsa.gov/main/'
+        assets = [{dest:'_includes/footer.html', filename:'_includes/navigation/footer.html'}]
+        u = URI(based_uri + assets[0].filename)
         Net::HTTP.start(u.host, u.port, :use_ssl => true) do |http|
             request = Net::HTTP::Get.new(u)
             http.request(request) do |response|
-            open("_includes/footer.html", "wb") do |file|
+            open(assets[0].filename, "wb") do |file|
               response.read_body do |chunk|
                 file.write(chunk)
               end
